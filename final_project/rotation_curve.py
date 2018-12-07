f6d01d3936724eb67809d33c4f44cf0d55c12a42
Ubuntu <ubuntu@ip-172-31-91-104.ec2.internal>
2018-12-07 16:06:51 +0000

    modify

--- final_project/rotation_curve.py
+++ final_project/rotation_curve.py
@@ -17,14 +17,14 @@ import Rappture
 import sys
 from math import *
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import dblquad
 from scipy import optimize
 
 def f(r, formula):
     return eval(formula)
 
-def calc(M, r, formula):
-    return eval(formula) * r**2
+def calc(M, r, theta, formula):
+    return eval(formula) * r**2 * sin(theta)
 
 def main():
     io = Rappture.library(sys.argv[1])
@@ -32,13 +32,14 @@ def main():
     rmin = float(io.get('input.number(min).current'))
     rmax = float(io.get('input.number(max).current'))
     M0 = float(io.get('input.number(M0).current'))
+#    theta = float(io.get('input.number(theta).current'))
     npts = int(io.get('input.number(Npts).current'))
     formula = io.get('input.string(formula).current')
-    #formula1 = '(' + io.get('input.string(formula).current')+')*r**2.'
 
     r = np.linspace(rmin, rmax, npts)
-    sol = odeint(calc, M0, r, args=(formula,))
+    #sol = dblquad(lambda: x,theta: eval(formula) * x**2 * sin(theta),0,np.pi,lambda x:rmin,r)
     v = np.linspace(0,npts,npts)
+
     io.put('output.curve(result2).about.label','Mass vs r',append=0)
     io.put('output.curve(result2).yaxis.label','Mass')
     io.put('output.curve(result2).xaxis.label','r')
@@ -50,9 +51,10 @@ def main():
     for i in range(npts):
         io.put(
                'output.curve(result2).component.xy',
-               '%g %g\n' % (r[i],sol[i][0]), append=1
+               '%g %g\n' % (r[i],dblquad(lambda theta,x: eval(formula) * x**2 *
+                sin(theta),0,np.pi,lambda x:rmin,r[i])[0]), append=1
               )
-        v[i] = (sol[i][0]/r[i])**0.5
+        v[i] = (dblquad(lambda theta,x: eval(formula) * x**2 * sin(theta),0,np.pi,lambda x:rmin,r[i])[i]/r[i])**0.5
         io.put(
                'output.curve(result3).component.xy',
                '%g %g\n' % (r[i],v[i]), append=1
